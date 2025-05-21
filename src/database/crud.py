797a0c8829f7b7dd10bca3f33660906b905a55a9
Dede <specialized8393@gmail.com>
2025-05-21 18:09:28 +0100

    feat: add create car DB

--- src/database/crud.py
+++ src/database/crud.py
@@ -6,3 +6,11 @@ from .models import Car
 async def get_car_by_url(db: AsyncSession, url: str):
     result = await db.execute(select(Car).where(Car.url == url))
     return result.scalars().first()
+
+
+async def create_car(db: AsyncSession, car_data: dict):
+    db_car = Car(**car_data)
+    db.add(car_data)
+    await db.commit()
+    await db.refresh(car_data)
+    return db_car
